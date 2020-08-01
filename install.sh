a0eb1a2e73e90145e2d15689fc7290ccec34e746
David Leininger <davidbleininger@gmail.com>
2020-07-31 22:55:43 -0500

    add hushlogin

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ ln -sf "$DOTFILES_DIR/.gitconfig" ~
 ln -sf "$DOTFILES_DIR/.gitconfig-dl" ~
 ln -sf "$DOTFILES_DIR/.gitconfig-wwt" ~
 ln -sf "$DOTFILES_DIR/git" ~/.config
+ln -sf "$DOTFILES_DIR/.hushlogin" ~
 ln -sf "$DOTFILES_DIR/.gitignore_global" ~
 ln -sf "$DOTFILES_DIR/.hyper.js" ~
 ln -sf "$DOTFILES_DIR/.zshrc" ~
