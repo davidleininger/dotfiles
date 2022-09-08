ab9d1a542deec228d43bd03fcde0ea98bb5f28e4
David Leininger <davidleininger@hey.com>
2022-09-08 16:56:58 -0500

    more robust install script

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-# # if they have a .config/fish, kill it
-# echo "Installing Oh My ZSH"
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# install zsh and zsh plugins
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/paulirish/git-open ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open
+
+# Remove old config files
+cd ~
+rm -rf ~/.config
+
+# got some things to do in dotfiles
+cd ~/dev/dotfiles
 
 # Get dotfiles installation directory
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -21,3 +30,26 @@ ln -sf "$DOTFILES_DIR/.zshrc" ~
 ln -sf "$DOTFILES_DIR/z.sh" ~
 ln -sf "$DOTFILES_DIR/leininger.zsh-theme" ~/.oh-my-zsh/custom/themes
 ln -sf "$DOTFILES_DIR/keybindings.json" ~/Library/Application\ Support/Code/User/keybindings.json
+
+# back to root
+cd ~
+
+# Get homebrew and bring that badboy in
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Add homebrew to PATH
+export PATH=/opt/homebrew/bin:$PATH
+export PATH=/opt/homebrew/sbin:$PATH
+
+# Do the homebrew bundle stuff
+brew tap Homebrew/bundle
+
+# Install global npm modules that we'll need
+npm i -g empty-trash-cli fkill-cli np trash-cli convert-color-cli yarn
+
+# install homebrew applications
+brew bundle
+
+# finally go back to dotfiles and npm install
+cd ~/dev/dotfiles
+npm install
